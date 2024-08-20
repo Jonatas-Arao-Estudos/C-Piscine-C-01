f339862da37cf1ac07fe01fc12218adb6a80ff16
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-20 19:18:01 -0300

    ex07 added

---
+++ ex07/ft_rev_int_tab.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 18:11:24 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/08/20 18:58:39 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	index;
+	int	temp;
+
+	index = 0;
+	while (index < (size / 2))
+	{
+		temp = tab[index];
+		tab[index] = tab[size - index - 1];
+		tab[size - index - 1] = temp;
+		index++;
+	}
+}
